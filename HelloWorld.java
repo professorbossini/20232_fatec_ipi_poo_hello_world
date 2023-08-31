ffd5bdad920e21bafbff75ecf05d4452d443ba52
Rodrigo Bossini <professorbossini@gmail.com>
2023-08-31 15:48:35 -0300

    ap01(java): hello world
    - Utilizando a linha de comando

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+  public static void main(String [] args){
+    //stdin stdout stderr
+    System.out.println("Hello, World");
+  }
+}
+//Em Java, o compilador produz bytecode
+//é a linguagem que a JVM entende
+
+//A JVM interpreta bytecode
